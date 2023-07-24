89280d60bcf2a35c0403c9f1091ef6fc386b0ae2
Abdullahi254 <abdush504@gmail.com>
2023-07-24 12:58:16 +0300

    made some betty complaint changes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,20 +9,28 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i, j;
+	unsigned int n, j;
 	int tmp;
+	unsigned int is_swapped;
 
-	for (i = size; i > 0; i--)
+	for (n = 0; n < size; n++)
 	{
-		for (j = 0; j < i - 1; j++)
+		is_swapped = 0;
+		for (j = 0; j < size - 1 - n; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
-			    print_array(array, size);
+				is_swapped = 1;
+				print_array(array, size);
 			}
+
+		}
+		if (!is_swapped)
+		{
+			break;
 		}
 	}
 }
