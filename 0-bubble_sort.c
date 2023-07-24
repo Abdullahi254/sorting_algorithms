c3f9fd1de28ab581cf4600e04b75dad884f56be5
Abdullahi254 <abdush504@gmail.com>
2023-07-24 12:59:10 +0300

    revoked previous changes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,28 +9,20 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int n, j;
+	unsigned int i, j;
 	int tmp;
-	unsigned int is_swapped;
 
-	for (n = 0; n < size; n++)
+	for (i = size; i > 0; i--)
 	{
-		is_swapped = 0;
-		for (j = 0; j < size - 1 - n; j++)
+		for (j = 0; j < i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
-				is_swapped = 1;
-				print_array(array, size);
+			    print_array(array, size);
 			}
-
-		}
-		if (!is_swapped)
-		{
-			break;
 		}
 	}
 }
